2e41769024c65f6e76b4c06eeb7d480055b7a510
honjun <hj531@live.com>
2019-03-31 12:30:36 +0800

    卜算子pjax适配，修复无图文章目录，修复无图文章一直呈现加载图为404图

--- themes/Sakura/source/js/sakura-app.js
+++ themes/Sakura/source/js/sakura-app.js
@@ -1048,17 +1048,31 @@ var home = location.href,
   Siren = {
     TOC: function () {
       if ($('.toc').length > 0 && document.body.clientWidth > 1200) {
-        tocbot.init({
-                    // Where to render the table of contents.
-          tocSelector: '.toc', // 放置目录的容器
-                    // Where to grab the headings to build the table of contents.
-          contentSelector: '.entry-content', // 正文内容所在
-                    // Which headings to grab inside of the contentSelector element.
-          scrollSmooth: true,
-          headingSelector: 'h1, h2, h3, h4, h5', // 需要索引的标题级别
-          headingsOffset: -400,
-          scrollSmoothOffset: -85
-        })
+        if ($(".pattern-center").length > 0) { //有图的情况
+          tocbot.init({
+              // Where to render the table of contents.
+              tocSelector: '.toc', // 放置目录的容器
+              // Where to grab the headings to build the table of contents.
+              contentSelector: '.entry-content', // 正文内容所在
+              // Which headings to grab inside of the contentSelector element.
+              scrollSmooth: true,
+              headingSelector: 'h1, h2, h3, h4, h5', // 需要索引的标题级别
+              headingsOffset: -400,
+              scrollSmoothOffset: -85
+          });
+        } else {
+          tocbot.init({
+              // Where to render the table of contents.
+              tocSelector: '.toc', // 放置目录的容器
+              // Where to grab the headings to build the table of contents.
+              contentSelector: '.entry-content', // 正文内容所在
+              // Which headings to grab inside of the contentSelector element.
+              scrollSmooth: true,
+              headingSelector: 'h1, h2, h3, h4, h5', // 需要索引的标题级别
+              headingsOffset: -85,
+              scrollSmoothOffset: -85
+          });
+        }
         var offsetTop = $('.toc').offset().top - 135
         window.onscroll = function () {
           var scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop
@@ -1068,6 +1082,7 @@ var home = location.href,
             $('.toc').removeClass('toc-fixed')
           }
         }
+        $.getScript('//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js');
       }
     },
     AB: function () {
